5954ae12e914a5f4c9b583e9e5518d490706757e
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-07-30 03:48:53 -0400

    adds variables for html collections of firstRow and secondRow

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let squareOne = document.getElementsByClassName("square-1");
 let squareTwo = document.getElementsByClassName('square-2');
 let squareThree = document.getElementsByClassName('square-3');
+let firstRow = document.getElementsByClassName('row-1');
+let secondRow = document.getElementsByClassName('row-2');
