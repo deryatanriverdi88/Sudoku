6a88237c06d402ac6cded51e3741d4aef86019a8
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-07-30 03:47:32 -0400

    adds variables for html collection of squareThree

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let squareOne = document.getElementsByClassName("square-1");
 let squareTwo = document.getElementsByClassName('square-2');
+let squareThree = document.getElementsByClassName('square-3');
